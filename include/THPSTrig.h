a8e2fd4405a4085b56c7bba8d01dffe9abd0588f
Rafayel Paremuzyan <rafopar@jlab.org>
2019-06-09 21:16:12 -0400

    As a first commit, Just an empty frame of the Trigger decoder

---
+++ include/THPSTrig.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   THPSTrig.h
+ * Author: rafopar
+ *
+ * Created on June 9, 2019, 9:14 PM
+ */
+
+#ifndef THPSTRIG_H
+#define THPSTRIG_H
+
+class THPSTrig {
+public:
+    THPSTrig();
+    THPSTrig(const THPSTrig& orig);
+    virtual ~THPSTrig();
+private:
+
+};
+
+#endif /* THPSTRIG_H */
+
---
+++ src/THPSTrig.cc
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   THPSTrig.cc
+ * Author: rafopar
+ * 
+ * Created on June 9, 2019, 9:14 PM
+ */
+
+#include "THPSTrig.h"
+
+THPSTrig::THPSTrig() {
+}
+
+THPSTrig::THPSTrig(const THPSTrig& orig) {
+}
+
+THPSTrig::~THPSTrig() {
+}
+

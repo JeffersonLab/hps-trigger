37f264844df8b9e41c6a6a453aa9642c659dfbb5
Rafayel Paremuzyan <rafopar@jlab.org>
2019-06-10 00:27:40 -0400

    Added CLuster Reading part, compiles fine, but didn't test yet on data, will continue tomorrow...

--- include/THPSTrig.h
+++ include/THPSTrig.h
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   THPSTrig.h
  * Author: rafopar
@@ -14,12 +8,71 @@
 #ifndef THPSTRIG_H
 #define THPSTRIG_H
 
+#include <evioUtil.hxx>
+#include <ap_int.h>
+#include <ap_fixed.h>
+#include <hls_stream.h>
+#include <vector>
+
+// ===== The HPS Calorimeter cluster object
+
+typedef struct {
+    int E;
+    int Y;
+    int X;
+    int N;
+    int T;
+} THPS_Cluster;
+
 class THPSTrig {
 public:
     THPSTrig();
-    THPSTrig(const THPSTrig& orig);
+    THPSTrig(evio::evioDOMNode*, int);
+    //THPSTrig(const THPSTrig& orig);
     virtual ~THPSTrig();
+
+    void SetevioDOMENodeSect(evio::evioDOMNode*, int);
+
+    int GetNClust() {return fn_HPS_Cl;}; // Returns number of clusters
+    int GetNTopClust() {return fn_HPS_Top_Cl;}; // Returns number of Top clusters
+    int GetNBotClust() {return fn_HPS_Bot_Cl;}; // Returns number of Top clusters
+    
+    THPS_Cluster *GetCLuster(int i);              // Get i-th cluster
+    THPS_Cluster *GetTopCLuster(int i);           // Get i-th Top cluster
+    THPS_Cluster *GetBotCLuster(int i);           // Get i-th Bot cluster
+
+
 private:
+
+    vector<THPS_Cluster> fv_HPSCL;
+    vector<int> fv_ind_HPS_TopCL; // Index of Top HPSClusters
+    vector<int> fv_ind_HPS_BotCL; // Index of Bot HPSClusters
+
+
+    bool has_HPSCl;
+
+    std::vector<ap_int<32> >::iterator fit_data;
+    evio::evioDOMNode* fhead_node; //pointer to the head node of VTP bank
+
+    static map<int, string> vtp_sect;
+    int fROC_ID;
+
+    int fn_HPS_Cl;          // # of all clusters
+    int fn_HPS_Top_Cl;      // # of top clusters
+    int fn_HPS_Bot_Cl;      // # of bot clusters
+       
+    static const int f_Top_rocID = 60011;
+    static const int f_Bot_rocID = 60011;
+
+    // ================= The 1st four bits are occupied and the rest of trigger objects will be defined using 
+    // ================= next four bits =========================================
+    static const unsigned short int type_switch2ndlvl = 12;
+    static const unsigned short int type_2ndlvl_HPSCL = 02; // HPS cluster type 
+
+    void ResetAll();
+    
+    void ReadHPSCL(); // This method will read HPSClusters
+    void Check2ndlvltype();
 
 };
 
--- src/THPSTrig.cc
+++ src/THPSTrig.cc
@@ -13,12 +13,143 @@
 
 #include "THPSTrig.h"
 
+using namespace std;
+
+map<int, string> THPSTrig::vtp_sect = {
+    {f_Top_rocID, "Top"},
+    {f_Bot_rocID, "Bot"}
+};
+
 THPSTrig::THPSTrig() {
 }
 
-THPSTrig::THPSTrig(const THPSTrig& orig) {
+THPSTrig::THPSTrig(evio::evioDOMNode* it, int a_rocID_tag) {
+    SetevioDOMENodeSect(it, a_rocID_tag);
 }
+
+//THPSTrig::THPSTrig(const THPSTrig& orig) {
+//}
+
+void THPSTrig::SetevioDOMENodeSect(evio::evioDOMNode* it, int a_rocID_tag) {
+
+    ResetAll();
+    fhead_node = it;
+
+    // ===== Make sure correct rocID is provided
+    if (!(a_rocID_tag == f_Top_rocID || a_rocID_tag == f_Bot_rocID)) {
+        printf("Wrong Bank Tag is provided %s Exiting \n", __func__);
+    }
+
+    fROC_ID = a_rocID_tag;
+
+
+    vector<ap_int<32> > *data_values = (vector<ap_int < 32 >> *)it->getVector<uint32_t>();
+
+    for (fit_data = data_values->begin(); fit_data != data_values->end(); fit_data++) {
+
+        ap_int<1> is_type_def = fit_data->range(31, 31);
+
+        if (is_type_def) {
+            ap_uint<4> data_type = fit_data->range(30, 27);
+
+            switch (data_type) {
+
+                case type_switch2ndlvl: Check2ndlvltype();
+                    break;
+
+            }
+        }
+
+
+        fn_HPS_Cl = fv_HPSCL.size();
+        fn_HPS_Top_Cl = fv_ind_HPS_TopCL.size();
+        fn_HPS_Bot_Cl = fv_ind_HPS_BotCL.size();
+    }
+
+
+
+}
+
+void THPSTrig::Check2ndlvltype() {
+
+    ap_uint<4> type_2ndlvl = fit_data->range(26, 23);
+
+    switch (type_2ndlvl) {
+
+        case type_2ndlvl_HPSCL: ReadHPSCL();
+            break;
+
+    }
+}
+
+void THPSTrig::ReadHPSCL() {
+    has_HPSCl = true;
+
+    THPS_Cluster cur_cl;
+
+    cur_cl.E = fit_data->range(22, 10);
+    cur_cl.Y = fit_data->range(9, 6);
+    cur_cl.X = fit_data->range(5, 0);
+
+    // ===== Goint to the next word =====
+    fit_data = std::next(fit_data, 1);
+    cur_cl.N = fit_data->range(13, 10);
+    cur_cl.T = fit_data->range(9, 0);
+
+    fv_HPSCL.push_back(cur_cl);
+
+    if (cur_cl.Y > 0) {
+        fv_ind_HPS_TopCL.push_back(fv_HPSCL.size() - 1);
+    } else if (cur_cl.Y < 0) {
+        fv_ind_HPS_BotCL.push_back(fv_HPSCL.size() - 1);
+    } else {
+        cout << "The HPS cluster Y is 0. This should not happen" << endl;
+        cout << "Exiting " << endl;
+        exit(1);
+    }
+}
+
+THPS_Cluster * THPSTrig::GetCLuster(int ind){
+
+    if( ind >= fn_HPS_Cl || ind < 0){
+        cout<<"Requested wrong cluster index="<<ind<<endl;
+        cout<<"# of clusters is "<<fn_HPS_Cl<<"    So he index should be between 0  and"<<(fn_HPS_Cl - 1)<<endl;
+    }
+    
+    return &fv_HPSCL.at(ind);
+}
+
+THPS_Cluster * THPSTrig::GetTopCLuster(int ind){
+
+    if( ind >= fn_HPS_Top_Cl || ind < 0){
+        cout<<"Requested wrong cluster index="<<ind<<endl;
+        cout<<"# of Top clusters is "<<fn_HPS_Top_Cl<<"    So he index should be between 0  and"<<(fn_HPS_Top_Cl - 1)<<endl;
+    }
+    
+    return &fv_HPSCL.at(fv_ind_HPS_TopCL.at(ind));
+}
+
 
 THPSTrig::~THPSTrig() {
 }
 
+void THPSTrig::ResetAll() {
+
+    has_HPSCl = false;
+
+    fv_HPSCL.clear();
+    fv_HPSCL.shrink_to_fit();
+
+    fv_ind_HPS_TopCL.clear();
+    fv_ind_HPS_TopCL.shrink_to_fit();
+
+    fv_ind_HPS_BotCL.clear();
+    fv_ind_HPS_BotCL.shrink_to_fit();
+
+
+    fn_HPS_Cl = 0;
+    fn_HPS_Top_Cl = 0;
+    fn_HPS_Bot_Cl = 0;
+
+
+}
